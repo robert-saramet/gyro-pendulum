302e5657664309e307da5048f9154d077ec261a5
Robert Saramet <robertsaramet@pm.me>
2022-11-13 12:51:24 +0200

    Write main and IMU loop

--- main.cpp
+++ main.cpp
@@ -8,19 +8,32 @@ Adafruit_Sensor *accelerometer, *gyroscope, *magnetometer;
 Adafruit_NXPSensorFusion filter;
 Adafruit_Sensor_Calibration_EEPROM cal;
 
-#define FILTER_UPDATE_RATE_HZ 200
-#define PRINT_EVERY_N_UPDATES 20
+#define FILTER_UPDATE_RATE_HZ 400
+#define PRINT_EVERY_N_UPDATES 8
 
 uint32_t timestamp;
 
-float roll, pitch, heading, offset, angle;
+float  pitch, offset, angle;
+
+#define TOUCH_PIN 2
+#define TOUCH_TRESHOLD 17
+
 bool serverMode = false;
 bool firstRun = true;
+int id = 0;
 
-void setup() {
-  Serial.begin(115200);
-  while (!Serial) yield();
+#define MAX_ROWS 10
+long t0[MAX_ROWS], t1[MAX_ROWS], tX[MAX_ROWS];
+float u0[MAX_ROWS], u1[MAX_ROWS], uX[MAX_ROWS];
+float dps[MAX_ROWS];
 
+#define MIN_MOVEMENT 2  //deg
+#define DIR_CHECK_INTERVAL 20  //ms
+
+long ledTime = millis();
+bool ledState = true;
+
+void setupIMU() {
   if (!cal.begin()) {
     Serial.println("Failed to initialize calibration helper");
   } else if (! cal.loadCalibration()) {
@@ -43,7 +56,22 @@ void setup() {
   Wire.setClock(400000); // 400KHz
 }
 
-void getAngles() {
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(TOUCH_PIN, INPUT);
+  setupIMU();
+}
+
+void blinkLed() {
+  if(millis() - ledTime > 1000) {
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState);
+    ledTime = millis();
+  }
+}
+
+void pollIMU() {
   float gx, gy, gz;
   static uint8_t counter = 0;
 
@@ -61,35 +89,77 @@ void getAngles() {
   cal.calibrate(mag);
   cal.calibrate(accel);
   cal.calibrate(gyro);
-  
+
   gx = gyro.gyro.x * SENSORS_RADS_TO_DPS;
   gy = gyro.gyro.y * SENSORS_RADS_TO_DPS;
   gz = gyro.gyro.z * SENSORS_RADS_TO_DPS;
-
+  
   filter.update(gx, gy, gz, 
                 accel.acceleration.x, accel.acceleration.y, accel.acceleration.z, 
                 mag.magnetic.x, mag.magnetic.y, mag.magnetic.z);
 
-  // only print the calculated output once in a while
   if (counter++ <= PRINT_EVERY_N_UPDATES) {
     return;
   }
   // reset the counter
   counter = 0;
 
-  roll = filter.getRoll();
   pitch = filter.getPitch();
-  heading = filter.getYaw();
 }
 
 void warmup() {
   long startTime = millis();
   while(millis() - startTime < 2000) {
-    getAngles();
+    pollIMU();
   }
   angle = 0;
   offset = pitch - angle;
   firstRun = false;
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+bool touched() {
+  return (touchRead(TOUCH_PIN) <= TOUCH_TRESHOLD);
+}
+
+float getAngle() {
+  pollIMU();
+  angle = pitch - offset;
+  return angle;
+}
+
+bool loopIMU() {
+  if(++id > MAX_ROWS) {
+    return 1;
+  }
+  
+  u0[id] = getAngle();
+  t0[id] = millis();
+
+  float temp = angle;
+  delay(DIR_CHECK_INTERVAL);
+  while(abs(getAngle()) < abs(temp) - MIN_MOVEMENT) {
+    temp = getAngle();
+    delay(DIR_CHECK_INTERVAL);
+  }
+  delay(3*DIR_CHECK_INTERVAL);
+  temp = getAngle();
+  while(abs(getAngle()) > abs(temp) - MIN_MOVEMENT) {
+    temp = getAngle();
+    delay(DIR_CHECK_INTERVAL);
+  }
+  
+  u1[id] = angle;
+  t1[id] = millis();
+  uX[id] = u1[id] - u0[id];
+  tX[id] = t1[id] - t0[id];
+  dps[id] = 1000/tX[id] * uX[id];
+  
+  return 0;
+}
+
+void loopServer() {
+  blinkLed();
 }
 
 void loop() {
@@ -98,8 +168,23 @@ void loop() {
     return;
   }
 
-  getAngles();
-  angle = pitch - offset;
-
-  Serial.println(angle);
+  if(serverMode) {
+    while(!touched()) {
+      loopServer();
+    }
+    serverMode = false;
+    while(touched()) {
+      delay(10);
+    }
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH);
+    id = 0;
+    while(!touched()) {
+      bool done = loopIMU();
+      if(done) {
+        break;
+      }
+    }
+    serverMode = true;
+  }
 }
